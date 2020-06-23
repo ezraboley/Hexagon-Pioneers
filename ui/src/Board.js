5611391f32cf17e6cc894982800e759f8e74475b
Ezra Boley <eboley@wisc.edu>
2020-06-23 00:14:27 -0500

    Added a way to fetch all three corners based on where the user clicks

--- ui/src/Board.js
+++ ui/src/Board.js
@@ -2,6 +2,19 @@ import React from 'react';
 import Hexagon from './Hexagon.js';
 import styled from 'styled-components';
 
+class Coordinate {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.type = "cube";
+    }
+
+    toString() {
+        return `${this.x},${this.y},${this.z}`;
+    }
+}
+
 export default class Board extends React.Component {
 
     START_X = 100;
@@ -65,8 +78,8 @@ export default class Board extends React.Component {
         if (this.state.board === undefined) return;
         let positions = Object.keys(this.state.board);
         let pathStrs = [];
-        console.log(positions);
         console.log(this.state.board);
+        let newBoard = this.state.board;
         positions.forEach((pos) => {
             let offset = 300;
             let {x,y,z} = this.state.board[pos].pos;
@@ -76,6 +89,7 @@ export default class Board extends React.Component {
                     (x-y) * Hexagon.WIDTH/2),
                 (offset + 
                     (z) * (Hexagon.HEIGHT * 0.75)));
+            newBoard[pos].points = points;
             pathStrs.push( 
                 <Hexagon
                     handleClick={this.clickHandler}
@@ -88,27 +102,65 @@ export default class Board extends React.Component {
         return pathStrs;
     }
 
+    pointIsEqual(p1, p2) {
+        let x1 = p1.x;
+        let x2 = p2.x;
+        let y1 = p1.y;
+        let y2 = p2.y;
+        
+        let margin = 5;
+
+
+        return (
+            (margin + x1 >= x2 && x2 >= x1 - margin)
+        && (margin + y1 >= y2 && y2 >= y1 - margin));
+    }
+
     finalizeCorner() {
         if (this.state.activeCorner.key === null) {
            return;
         }
 
-        let pos = this.state.activeCorner.key;
+        let vals = this.state.activeCorner.key.split(',');
+        
+        let pos = {
+            x: parseInt(vals[0]), 
+            y: parseInt(vals[1]), 
+            z: parseInt(vals[2])
+        };//this.state.activeCorner.key;
+        let cornerX = this.state.activeCorner.x;
+        let cornerY = this.state.activeCorner.y;
 
         this.neighbors = [
-            {x: pos.x + 1, y: pos.y - 1, z: pos.z},
-            {x: pos.x + 1, y: pos.y, z: pos.z - 1},
-            {x: pos.x, y: pos.y + 1, z: pos.z - 1},
-            {x: pos.x - 1, y: pos.y + 1, z: pos.z},
-            {x: pos.x - 1, y: pos.y, z: pos.z + 1},
-            {x: pos.x, y: pos.y - 1, z: pos.z + 1},
-       ];
-      
+            new Coordinate(pos.x + 1, pos.y - 1, pos.z),
+            new Coordinate(pos.x + 1, pos.y,pos.z - 1),
+            new Coordinate(pos.x, pos.y + 1,pos.z - 1),
+            new Coordinate(pos.x - 1, pos.y + 1, pos.z),
+            new Coordinate(pos.x - 1, pos.y, pos.z + 1),
+            new Coordinate(pos.x, pos.y - 1, pos.z + 1),
+        ];
+        
+        let corners = [];
+        corners.push(new Coordinate(pos.x, pos.y, pos.z));
+
+        // Terrible, I know
+        this.neighbors.forEach((n) => {
+            if (this.state.board[n] === undefined) return;
+            for (let p of this.state.board[n].points) {
+                // Gotta do some fuzzy matching
+                if (this.pointIsEqual(p, {x: cornerX, y: cornerY})) {
+                    corners.push(n);
+                    break;
+                }
+            } 
+        });
+        console.log(corners);
     }
 
     render() {
         let board = this.buildBoard()
         if (board === undefined) return <p>end</p>;
+        this.finalizeCorner();
         return (
             <BoardContainer>
                 <BoardSpan/>
